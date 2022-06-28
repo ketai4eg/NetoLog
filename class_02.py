95ba9a9d7cc8eb8ae8bb5fd760d8ce7ee851a27b
Ketai4eg <starikevichm.other@gmail.com>
2022-06-28 10:58:31 +0100

    2

---
+++ class_02.py
@@ -0,0 +1,15 @@
+height = 180
+age =19
+children=2
+study=1
+if children>1:
+    print("go to your wife and help")
+if study:
+    print("go to classes")
+if 18<age<27:
+    if height < 170:
+        print("В танкисты")
+    else:
+        print("в другие войска")
+else:
+    print("go home")
