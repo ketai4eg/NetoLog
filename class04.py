1abb2852e0365d320946a057285506b8f48c7493
Ketai4eg <starikevichm.other@gmail.com>
2022-06-29 11:11:00 +0100

    2

---
+++ class04.py
@@ -0,0 +1,19 @@
+import pprint
+geo_logs = [
+    {'visit1': ['Москва', 'Россия']},
+    {'visit2': ['Дели', 'Индия']},
+    {'visit3': ['Владимир', 'Россия']},
+    {'visit4': ['Лиссабон', 'Португалия']},
+    {'visit5': ['Париж', 'Франция']},
+    {'visit6': ['Лиссабон', 'Португалия']},
+    {'visit7': ['Тула', 'Россия']},
+    {'visit8': ['Тула', 'Россия']},
+    {'visit9': ['Курск', 'Россия']},
+    {'visit10': ['Архангельск', 'Россия']}
+]
+geo_logs_up=[]
+for items in geo_logs:
+    for key, val in items.items():
+        if "Россия" in val:
+            geo_logs_up.append({key:val})
+pprint.pp(geo_logs_up)
